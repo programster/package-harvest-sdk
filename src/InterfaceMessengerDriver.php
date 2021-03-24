aea0e16225fe030c8694cd0dd3f1779546eaf14f
Programster <programster@users.noreply.github.com>
2021-03-24 15:20:36 +0000

    Adds function docblocks.
    Adds function docblocks to explain whats required of the methods.

--- src/InterfaceMessengerDriver.php
+++ src/InterfaceMessengerDriver.php
@@ -8,7 +8,17 @@ namespace Programster\Harvest;
 
 interface InterfaceMessengerDriver
 {
+    /**
+     * Create a barebones new request object that implements the Request Interface.
+     * We will use the interface's methods to "build it up" before sending. 
+     * @return \Psr\Http\Message\RequestInterface;
+     */
     public function newRequest() : \Psr\Http\Message\RequestInterface;
 
+    /**
+     * Convert a URI string to an object that implements the UriInterface
+     * @param string $uri - The URI to convert. E.g. https://www.google.com/search?q=programster
+     * @return \Psr\Http\Message\UriInterface - an object that implements this interface.
+     */
     public function convertStringToUri(string $uri) : \Psr\Http\Message\UriInterface;
 }
